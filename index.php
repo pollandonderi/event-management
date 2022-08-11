035abfd869ec29c9acff99b15d38e75b0ce14793
[polland] <[pollandsabisa@gmail.com]>
2022-08-11 09:37:42 +0300

    fistt committed

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/css1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans+Inline+One:ital@1&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kanfetti Event Planners</title>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light">
+                <a href="home.php" class="navbar-brand mb-0">
+                    <img src="./images/eventlogo.jpeg" class="d-inline-block align-top" width="40" height="30" alt="">
+
+                    Kanfetti Events
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+
+                </button>
+                <div class=" nav justify-content-center">
+
+                    <form class="form-inline">
+                        <input class=" mr-sm-2" type="search" placeholder="what are you looking for?" aria-label="Search">
+                        <button class="btn btn-outline-" type="submit">
+                            <i class="fa fa-search" style="font-size:24px"></i>
+                        </button>
+                    </form>
+
+                </div>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class=" nav-item active">
+                            <a href="#" class="nav-link active">
+                                home
+                            </a>
+                        </li>
+                        <li class=" nav-item">
+                            <a href="#" class="nav-link">
+                                about us
+                            </a>
+                        </li>
+
+                        <li class=" nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="dropdownNav" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                more
+                                <ul class="dropdown-menu" aria-labelledby="dropdowNav">
+                                    <li class="dropdown-item"><a href="#" class="dropdown-item">here</a></li>
+                                    <li class="dropdown-item"><a href="#" class="dropdown-item">here</a></li>
+                                    <li class="dropdown-item"><a href="#" class="dropdown-item">here</a></li>
+
+                                </ul>
+                            </a>
+                        </li>
+                        <li class=" nav-item">
+                            <a href="#" class="nav-link">
+                                contact us
+                            </a>
+                        </li>
+                        <li class=" nav-item">
+                            <a href="#" class="nav-link">
+                                blog
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="row">
+            <h1>...</h1>
+        </div>
+        <div class="row coverpage">
+            <img src="images/cover.jpg" alt="" srcset="" style="height: 600px;">
+            <div class="text text-white">
+                <h1 class="m-4">for your events</h1>
+                <h1 class="text-dark m-4">we,ve got you</h1>
+                <h1 class="text-dark m-4">we'll satisfy you</h1>
+                <button type="button" class="btn btn-dark m-4">Join us</button>
+            </div>
+        </div>
+        <div class="container carol" style="background:linear-gradient(-100deg,white 43%, #ffa54f 50%);">
+        <div class="row ">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row ">
+                            <div class="col-sm-6 col-md-5 col-lg-4 text-center">
+                                <div class="card h-100" >
+                                    <div class="row d-flex justify-content-center">
+                                    <img src="images/merub.jpeg" height="200" class="card-img-top cardrad" alt="..." style="width: 200px;">
+                                    </div>
+                                    
+                                    <div class="card-body">
+                                        <h5 class="card-title">Merub says,</h5>
+                                        <p class="card-text"><i class="fa fa-quote-left"></i>Some quick example text to build on the card title and make up the bulk of the card's content.<i class="fa fa-quote-right"></i></p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-5 col-lg-4 text-center">
+                            <div class="card h-100" >
+                            <div class="row d-flex justify-content-center">
+                                    <img src="images/helvin.JPG" height="200" class="card-img-top cardrad" alt="..." style="width: 200px;">
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text"><i class="fa fa-quote-left"></i>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-5 col-lg-4 text-center">
+                            <div class="card h-100" >
+                            <div class="row d-flex justify-content-center">
+                                    <img src="images/polland.JPG" height="200" class="card-img-top cardrad" alt="..." style="width: 200px;">
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Merub says</h5>
+                                        <p class="card-text"><i class="fa fa-quote-left"></i>Some quick example text to build on the card title and make up the bulk of the card's content.<i class="fa fa-quote-right"></i></p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="container-fluid">
+                <div class="row  d-flex align-items-center">
+                    <div class="col-sm-6 col-md-6 col-lg-6 ">
+                        <!-- <img src="../images/whateverplan.jpg" alt="planning" srcset="" class="covered" style="background-size:cover ;"> -->
+                        <div class="card text-center h-100" style="width: 100%;">
+                            <div class="card-body ">
+                                <h5 class="card-title text-center">In Your every dream,</h5>
+                                <p class="card-text">We make it a reality so you do not have to dream anymore</p>
+                                <a href="#" class="btn btn-secondary">Connect with our team</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <div class="card  style=" outline: none;"">
+                            <div class="card-body carding" style="height: 300px;">
+                                <!-- <img src="images/team.jpg" class=" m-5 justify-content-center" alt="" srcset="" height="400" width="78"> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
